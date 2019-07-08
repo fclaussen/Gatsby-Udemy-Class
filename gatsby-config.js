a9742f8da59dad5f0a784f7256c3d4b724bb0551
Fernando Claussen <fernandoclaussen@gmail.com>
2019-07-07 23:21:10 -0400

    Adding netlify gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
