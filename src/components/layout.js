69edee3d005b66760afccf8863e556c1663a1d39
Fernando Claussen <fernandoclaussen@gmail.com>
2019-07-07 23:26:54 -0400

    Changing font-display

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ import {graphql, StaticQuery} from 'gatsby'
 
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=optional');
   body {
     font-family: 'Open Sans', sans-serif;
     margin: 0 !important;
